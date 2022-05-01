605ca11340e7b257a97e0ed312adf4683d4d4075
SSK137 <saurabhkodam@gmail.com>
2022-05-01 12:03:45 +0530

    AccessModifier and Operators : reverse Number using For

---
+++ src/For_Loop_Code/Reverse_Integer.java
@@ -0,0 +1,15 @@
+package For_Loop_Code;
+
+public class Reverse_Integer {
+    public static void main(String[] args) {
+        int number=231;
+        int remainder=0;
+        int reverse=0;
+        for(int i=1;i<=3;i++){
+            remainder=number%10;
+            reverse=(reverse*10)+remainder;
+            number=number/10;
+        }
+        System.out.println(reverse);
+    }
+}
