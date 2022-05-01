fcf7ff37c850e99d278ee502fb708910a66d70ec
SSK137 <saurabhkodam@gmail.com>
2022-05-01 12:01:36 +0530

    AccessModifier and Operators : Sum of natural number using For

---
+++ src/For_Loop_Code/SumofNnaturalNUmber.java
@@ -0,0 +1,11 @@
+package For_Loop_Code;
+
+public class SumofNnaturalNUmber {
+    public static void main(String[] args) {
+        int sum=0;
+        for (int i=1;i<=5;i++){
+            sum=sum+i;
+        }
+        System.out.println("Sum ="+sum);
+    }
+}
